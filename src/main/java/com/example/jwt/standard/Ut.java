fcb33b8c83e3139a59979b96d507decceffd7aa8
asungkim <kimasung143@gmail.com>
2025-02-07 15:35:39 +0900

    jwt util getPayLoad

--- src/main/java/com/example/jwt/standard/Ut.java
+++ src/main/java/com/example/jwt/standard/Ut.java
@@ -4,19 +4,19 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 import java.util.Map;
 
 public class Ut {
     public static class Json {
-        private static final ObjectMapper objectMapper=new ObjectMapper();
+        private static final ObjectMapper objectMapper = new ObjectMapper();
 
         public static String toString(Object obj) {
             try {
                 return objectMapper.writeValueAsString(obj);
-            }
-            catch (JsonProcessingException e) {
+            } catch (JsonProcessingException e) {
                 throw new RuntimeException(e);
             }
         }
@@ -24,7 +24,7 @@ public class Ut {
 
     public static class Jwt {
 
-        public static String createToken(Key secretKey, int expireSeconds, Map<String,Object> claims) {
+        public static String createToken(Key secretKey, int expireSeconds, Map<String, Object> claims) {
             Date issuedAt = new Date();
             Date expiration = new Date(issuedAt.getTime() + 1000L * expireSeconds);
 
@@ -35,6 +35,29 @@ public class Ut {
                     .expiration(expiration)
                     .signWith(secretKey)
                     .compact();
+        }
+
+        public static boolean isValidToken(SecretKey secretKey, String token) {
+            try {
+                Jwts
+                        .parser()
+                        .verifyWith(secretKey)
+                        .build()
+                        .parse(token);
+            } catch (Exception e) {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static Map<String, Object> getPayLoad(SecretKey secretKey, String token) {
+            return (Map<String, Object>) Jwts
+                    .parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parse(token)
+                    .getPayload();
         }
     }
 }
