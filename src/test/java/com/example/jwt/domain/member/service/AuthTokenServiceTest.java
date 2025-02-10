d5339d9c9c24e06f95f74a327a1ae63b259110d3
asungkim <asungkim007@naver.com>
2025-02-10 10:49:15 +0900

    custom.jwt.value

--- src/test/java/com/example/jwt/domain/member/service/AuthTokenServiceTest.java
+++ src/test/java/com/example/jwt/domain/member/service/AuthTokenServiceTest.java
@@ -8,6 +8,7 @@ import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +28,11 @@ public class AuthTokenServiceTest {
     @Autowired
     private MemberService memberService;
 
+    @Value("${custom.jwt.secret-key}")
+    private String keyString;
+    @Value("${custom.jwt.expire-seconds}")
+    private int expireSeconds;
+
     SecretKey secretKey = Keys.hmacShaKeyFor("abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes());
 
     @Test
@@ -40,10 +46,10 @@ public class AuthTokenServiceTest {
     void createToken() {
         int expireSeconds = 60 * 60 * 24 * 365;
         Map<String, Object> original = Map.of("name", "paul", "age", 23);
-        String jwtStr = Ut.Jwt.createToken(secretKey, expireSeconds, original);
+        String jwtStr = Ut.Jwt.createToken(keyString, expireSeconds, original);
 
         assertThat(jwtStr).isNotBlank();
-        Map<String, Object> parsePayload = Ut.Jwt.getPayload(secretKey, jwtStr);
+        Map<String, Object> parsePayload = Ut.Jwt.getPayload(keyString, jwtStr);
 
         assertThat(parsePayload).containsAllEntriesOf(original);
 
@@ -67,10 +73,10 @@ public class AuthTokenServiceTest {
         Member member = memberService.findByUsername("user1").get();
         String accessToken = authTokenService.genAccessToken(member);
 
-        boolean isValid = Ut.Jwt.isValidToken(secretKey, accessToken);
+        boolean isValid = Ut.Jwt.isValidToken(keyString, accessToken);
         assertThat(isValid).isTrue();
 
-        Map<String, Object> parsedPayLoad = authTokenService.getPayload(secretKey, accessToken);
+        Map<String, Object> parsedPayLoad = authTokenService.getPayload(accessToken);
 
         assertThat(parsedPayLoad).containsAllEntriesOf(
                 Map.of("id", member.getId(), "username", member.getUsername())
