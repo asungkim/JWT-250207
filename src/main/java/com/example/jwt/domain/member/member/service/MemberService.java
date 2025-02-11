b407cfb1f28861c0cf1df407bb9e02a973b82cf9
asungkim <asungkim007@naver.com>
2025-02-11 11:16:57 +0900

    split authToken, apiKey add at genAccessToken by Member

--- src/main/java/com/example/jwt/domain/member/member/service/MemberService.java
+++ src/main/java/com/example/jwt/domain/member/member/service/MemberService.java
@@ -43,7 +43,7 @@ public class MemberService {
     }
 
     public String getAuthToken(Member member) {
-        return authTokenService.genAccessToken(member);
+        return member.getApiKey() + " " + authTokenService.genAccessToken(member);
     }
 
     public Optional<Member> getMemberByAccessToken(String accessToken) {
